a049f659b91981da8c24cbec73f0a74629f1ad3c
Ian Wrzesinski <wrzian@umich.edu>
2023-07-12 03:17:07 -0500

    whitespace: update verilog source whitespace

--- verilog/regfile.sv
+++ verilog/regfile.sv
@@ -13,44 +13,44 @@
 `include "sys_defs.svh"
 
 module regfile (
-	input [4:0]       rda_idx, rdb_idx, wr_idx, // read/write index
-	input [`XLEN-1:0] wr_data,                  // write data
-	input             wr_en, wr_clk,
+    input [4:0]       rda_idx, rdb_idx, wr_idx, // read/write index
+    input [`XLEN-1:0] wr_data,                  // write data
+    input             wr_en, wr_clk,
 
-	output logic [`XLEN-1:0] rda_out, rdb_out   // read data
+    output logic [`XLEN-1:0] rda_out, rdb_out   // read data
 );
 
-	logic [31:0] [`XLEN-1:0] registers; // 32, 64-bit Registers
+    logic [31:0] [`XLEN-1:0] registers; // 32, 64-bit Registers
 
-	wire [`XLEN-1:0] rda_reg = registers[rda_idx];
-	wire [`XLEN-1:0] rdb_reg = registers[rdb_idx];
+    wire [`XLEN-1:0] rda_reg = registers[rda_idx];
+    wire [`XLEN-1:0] rdb_reg = registers[rdb_idx];
 
-	// Read port A
-	always_comb begin
-		if (rda_idx == `ZERO_REG)
-			rda_out = 0;
-		else if (wr_en && (wr_idx == rda_idx))
-			rda_out = wr_data;  // internal forwarding
-		else
-			rda_out = rda_reg;
-	end
+    // Read port A
+    always_comb begin
+        if (rda_idx == `ZERO_REG)
+            rda_out = 0;
+        else if (wr_en && (wr_idx == rda_idx))
+            rda_out = wr_data;  // internal forwarding
+        else
+            rda_out = rda_reg;
+    end
 
-	// Read port B
-	always_comb begin
-		if (rdb_idx == `ZERO_REG)
-			rdb_out = 0;
-		else if (wr_en && (wr_idx == rdb_idx))
-			rdb_out = wr_data;  // internal forwarding
-		else
-		  rdb_out = rdb_reg;
-	end
+    // Read port B
+    always_comb begin
+        if (rdb_idx == `ZERO_REG)
+            rdb_out = 0;
+        else if (wr_en && (wr_idx == rdb_idx))
+            rdb_out = wr_data;  // internal forwarding
+        else
+          rdb_out = rdb_reg;
+    end
 
-	// Write port
-	always_ff @(posedge wr_clk) begin
-		if (wr_en) begin
-			registers[wr_idx] <= `SD wr_data;
-		end
-	end
+    // Write port
+    always_ff @(posedge wr_clk) begin
+        if (wr_en) begin
+            registers[wr_idx] <= `SD wr_data;
+        end
+    end
 
 endmodule // regfile
 
