cc275f4013b9ab7d3beb989075309d90ef78414c
Ian Wrzesinski <wrzian@umich.edu>
2023-08-16 10:07:00 -0700

    MEM stage: small comment changes

--- verilog/stage_mem.sv
+++ verilog/stage_mem.sv
@@ -19,16 +19,17 @@ module stage_mem (
     input [`XLEN-1:0]   Dmem2proc_data,
 
     output MEM_WB_PACKET     mem_packet,
-    output logic [1:0]       proc2Dmem_command, // the memory command
-    output MEM_SIZE          proc2Dmem_size,    // size of data to read or write
-    output logic [`XLEN-1:0] proc2Dmem_addr,    // Address sent to data-memory
-    output logic [`XLEN-1:0] proc2Dmem_data     // Data sent to data-memory
+    output logic [1:0]       proc2Dmem_command, // The memory command
+    output MEM_SIZE          proc2Dmem_size,    // Size of data to read or write
+    output logic [`XLEN-1:0] proc2Dmem_addr,    // Address sent to Data memory
+    output logic [`XLEN-1:0] proc2Dmem_data     // Data sent to Data memory
 );
 
     logic [`XLEN-1:0] read_data;
 
     assign mem_packet.result = (ex_mem_reg.rd_mem) ? read_data : ex_mem_reg.alu_result;
 
+    // Pass-throughs
     assign mem_packet.NPC          = ex_mem_reg.NPC;
     assign mem_packet.valid        = ex_mem_reg.valid;
     assign mem_packet.halt         = ex_mem_reg.halt;
@@ -36,11 +37,12 @@ module stage_mem (
     assign mem_packet.dest_reg_idx = ex_mem_reg.dest_reg_idx;
     assign mem_packet.take_branch  = ex_mem_reg.take_branch;
 
+    // Outputs from the processor to memory
     assign proc2Dmem_command = (ex_mem_reg.valid && ex_mem_reg.wr_mem) ? BUS_STORE :
                                (ex_mem_reg.valid && ex_mem_reg.rd_mem) ? BUS_LOAD : BUS_NONE;
     assign proc2Dmem_size = ex_mem_reg.mem_size;
     assign proc2Dmem_data = ex_mem_reg.rs2_value;
-    assign proc2Dmem_addr = ex_mem_reg.alu_result; // memory address is calculated by the ALU
+    assign proc2Dmem_addr = ex_mem_reg.alu_result; // Memory address is calculated by the ALU
 
     // Read data from memory and sign extend the proper bits
     always_comb begin
