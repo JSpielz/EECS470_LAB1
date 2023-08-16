85cde21f945c93dd58b0557edd4246e1033b4820
Ian Wrzesinski <wrzian@umich.edu>
2023-08-16 10:37:05 -0700

    ID stage: comments and some whitespace

--- verilog/stage_id.sv
+++ verilog/stage_id.sv
@@ -27,13 +27,10 @@ module decoder (
     output logic          illegal // non-zero on an illegal instruction
 );
 
-    // note: I recommend using an IDE's code folding feature on this block
+    // Note: I recommend using an IDE's code folding feature on this block
     always_comb begin
-        // default control values:
-        // - valid instructions must override these defaults as necessary.
-        // - opa_select, opb_select, and alu_func should be set explicitly.
-        // - These defaults are equivalent to a noop
-        // * see sys_defs.svh for the constants used here
+        // Default control values (looks like a NOP)
+        // See sys_defs.svh for the constants used here
         opa_select    = OPA_IS_RS1;
         opb_select    = OPB_IS_RS2;
         alu_func      = ALU_ADD;
@@ -202,8 +199,8 @@ endmodule // module decoder
 
 
 module stage_id (
-    input              clock,          // system clock
-    input              reset,          // system reset
+    input              clock,           // system clock
+    input              reset,           // system reset
     input IF_ID_PACKET if_id_reg,
     input              wb_regfile_en,   // Reg write enable from WB Stage
     input [4:0]        wb_regfile_idx,  // Reg write index from WB Stage
@@ -216,18 +213,17 @@ module stage_id (
     assign id_packet.PC   = if_id_reg.PC;
     assign id_packet.NPC  = if_id_reg.NPC;
 
-    // for counting valid instructions executed
-    // and for making the fetch stage die on halts/keeping track of when
+    // For counting valid instructions executed
+    // and making the fetch stage die on halts/keeping track of when
     // to allow the next instruction out of fetch
     // 0 for HALT and illegal instructions (end processor on halt)
     assign id_packet.valid = if_id_reg.valid & ~id_packet.illegal;
 
     logic has_dest_reg;
-    assign id_packet.dest_reg_idx =
-        (has_dest_reg) ? if_id_reg.inst.r.rd : `ZERO_REG;
+    assign id_packet.dest_reg_idx = (has_dest_reg) ? if_id_reg.inst.r.rd : `ZERO_REG;
 
-    // instantiate the register file
-    regfile regf_0 (
+    // Instantiate the register file
+    regfile regfile_0 (
         .clock  (clock),
         .read_idx_1 (if_id_reg.inst.r.rs1),
         .read_idx_2 (if_id_reg.inst.r.rs2),
@@ -240,7 +236,7 @@ module stage_id (
 
     );
 
-    // instantiate the instruction decoder
+    // Instantiate the instruction decoder
     decoder decoder_0 (
         // Inputs
         .inst  (if_id_reg.inst),
