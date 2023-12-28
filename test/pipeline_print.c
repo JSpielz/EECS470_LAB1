5aeca979effc243b36ad6073b3d405a5a4b1ce35
Ian Wrzesinski <wrzian@umich.edu>
2023-12-28 01:29:49 -0500

    pipe_print: break-out decode_inst() function

--- test/pipeline_print.c
+++ test/pipeline_print.c
@@ -11,7 +11,109 @@
 
 #include <stdio.h>
 
-#define NOOP_INST 0x00000013
+char* decode_inst(int inst)
+{
+    if (inst == 0x00000013) // NOOP instruction (r0 = r0 + r0)
+        return "nop";
+
+    int opcode = inst & 0x7f;
+    int funct3 = (inst >> 12) & 0x7;
+    int funct7 = inst >> 25;
+    int funct12 = inst >> 20; // for system instructions
+
+    // See the RV32I base instruction set table
+    switch (opcode) {
+        case 0x37: return "lui";
+        case 0x17: return "auipc";
+        case 0x6f: return "jal";
+        case 0x67: return "jalr";
+        case 0x63: // branch
+            switch (funct3) {
+                case 0b000: return "beq";
+                case 0b001: return "bne";
+                case 0b100: return "blt";
+                case 0b101: return "bge";
+                case 0b110: return "bltu";
+                case 0b111: return "bgeu";
+                default: return "unknown";
+            }
+        case 0x03: // load
+            switch (funct3) {
+                case 0b000: return "lb";
+                case 0b001: return "lh";
+                case 0b010: return "lw";
+                case 0b100: return "lbu";
+                case 0b101: return "lhu";
+                default: return "unknown";
+            }
+        case 0x23: // store
+            switch (funct3) {
+                case 0b000: return "sb";
+                case 0b001: return "sh";
+                case 0b010: return "sw";
+                default: return "unknown";
+            }
+        case 0x13: // immediate
+            switch (funct3) {
+                case 0b000: return "addi";
+                case 0b010: return "slti";
+                case 0b011: return "sltiu";
+                case 0b100: return "xori";
+                case 0b110: return "ori";
+                case 0b111: return "andi";
+                case 0b001:
+                    if (funct7 == 0x00) return "slli";
+                    return "unknown";
+                case 0b101:
+                    if (funct7 == 0x00) return "srli";
+                    if (funct7 == 0x20) return "srai";
+                    return "unknown";
+            }
+        case 0x33: // arithmetic
+            switch (funct7 << 4 | funct3) {
+                case 0x000: return "add";
+                case 0x200: return "sub";
+                case 0x001: return "sll";
+                case 0x002: return "slt";
+                case 0x003: return "sltu";
+                case 0x004: return "xor";
+                case 0x005: return "srl";
+                case 0x205: return "sra";
+                case 0x006: return "or";
+                case 0x007: return "and";
+                // M extension
+                case 0x010: return "mul";
+                case 0x011: return "mulh";
+                case 0x012: return "mulhsu";
+                case 0x013: return "mulhu";
+                case 0x014: return "div";  // unimplemented
+                case 0x015: return "divu"; // unimplemented
+                case 0x016: return "rem";  // unimplemented
+                case 0x017: return "remu"; // unimplemented
+                default: return "unknown";
+            }
+        case 0x0f: return "fence"; // unimplemented, imprecise
+        case 0x73:
+            switch (funct3) {
+                case 0b000:
+                    // unimplemented, somewhat inaccurate :(
+                    switch (funct12) {
+                        case 0x000: return "ecall";
+                        case 0x001: return "ebreak";
+                        case 0x105: return "wfi"; // we just mostly care about this
+                        default: return "system";
+                    }
+                case 0b001: return "csrrw";
+                case 0b010: return "csrrs";
+                case 0b011: return "csrrc";
+                case 0b101: return "csrrwi";
+                case 0b110: return "csrrsi";
+                case 0b111: return "csrrci";
+                default: return "unknown";
+            }
+        default: return "unknown";
+    }
+}
 
 static int cycle_count = 0;
 static FILE* ppfile = NULL;
@@ -38,120 +140,12 @@ void print_cycles()
 
 void print_stage(char* div, int inst, int npc, int valid_inst)
 {
-    int opcode, funct3, funct7, funct12;
     char *str;
 
     if (!valid_inst)
         str = "-";
-    else if (inst==NOOP_INST)
-        str = "nop";
-    else {
-        opcode = inst & 0x7f;
-        funct3 = (inst>>12) & 0x7;
-        funct7 = inst>>25;
-        funct12 = inst>>20; // for system instructions
-        // See the RV32I base instruction set table
-        switch (opcode) {
-        case 0x37: str = "lui"; break;
-        case 0x17: str = "auipc"; break;
-        case 0x6f: str = "jal"; break;
-        case 0x67: str = "jalr"; break;
-        case 0x63: // branch
-            switch (funct3) {
-            case 0b000: str = "beq"; break;
-            case 0b001: str = "bne"; break;
-            case 0b100: str = "blt"; break;
-            case 0b101: str = "bge"; break;
-            case 0b110: str = "bltu"; break;
-            case 0b111: str = "bgeu"; break;
-            default: str = "unknown"; break;
-            }
-            break;
-        case 0x03: // load
-            switch (funct3) {
-            case 0b000: str = "lb"; break;
-            case 0b001: str = "lh"; break;
-            case 0b010: str = "lw"; break;
-            case 0b100: str = "lbu"; break;
-            case 0b101: str = "lhu"; break;
-            default: str = "unknown"; break;
-            }
-            break;
-        case 0x23: // store
-            switch (funct3) {
-            case 0b000: str = "sb"; break;
-            case 0b001: str = "sh"; break;
-            case 0b010: str = "sw"; break;
-            default: str = "unknown"; break;
-            }
-            break;
-        case 0x13: // immediate
-            switch (funct3) {
-            case 0b000: str = "addi"; break;
-            case 0b010: str = "slti"; break;
-            case 0b011: str = "sltiu"; break;
-            case 0b100: str = "xori"; break;
-            case 0b110: str = "ori"; break;
-            case 0b111: str = "andi"; break;
-            case 0b001:
-                if (funct7 == 0x00) str = "slli";
-                else str = "unknown";
-                break;
-            case 0b101:
-                if (funct7 == 0x00) str = "srli";
-                else if (funct7 == 0x20) str = "srai";
-                else str = "unknown";
-                break;
-            }
-            break;
-        case 0x33: // arithmetic
-            switch (funct7 << 4 | funct3) {
-            case 0x000: str = "add"; break;
-            case 0x200: str = "sub"; break;
-            case 0x001: str = "sll"; break;
-            case 0x002: str = "slt"; break;
-            case 0x003: str = "sltu"; break;
-            case 0x004: str = "xor"; break;
-            case 0x005: str = "srl"; break;
-            case 0x205: str = "sra"; break;
-            case 0x006: str = "or"; break;
-            case 0x007: str = "and"; break;
-            // M extension
-            case 0x010: str = "mul"; break;
-            case 0x011: str = "mulh"; break;
-            case 0x012: str = "mulhsu"; break;
-            case 0x013: str = "mulhu"; break;
-            case 0x014: str = "div"; break;  // unimplemented
-            case 0x015: str = "divu"; break; // unimplemented
-            case 0x016: str = "rem"; break;  // unimplemented
-            case 0x017: str = "remu"; break; // unimplemented
-            default: str = "unknown"; break;
-            }
-            break;
-        case 0x0f: str = "fence"; break; // unimplemented, imprecise
-        case 0x73:
-            switch (funct3) {
-            case 0b000:
-                // unimplemented, somewhat inaccurate :(
-                switch (funct12) {
-                case 0x000: str = "ecall"; break;
-                case 0x001: str = "ebreak"; break;
-                case 0x105: str = "wfi"; break; // we just mostly care about this
-                default: str = "system"; break;
-                }
-                break;
-            case 0b001: str = "csrrw"; break;
-            case 0b010: str = "csrrs"; break;
-            case 0b011: str = "csrrc"; break;
-            case 0b101: str = "csrrwi"; break;
-            case 0b110: str = "csrrsi"; break;
-            case 0b111: str = "csrrci"; break;
-            default: str = "unknown"; break;
-            }
-            break;
-        default: str = "unknown"; break;
-        }
-    }
+    else
+        str = decode_inst(inst);
 
     if (ppfile != NULL)
         fprintf(ppfile, "%s%4d:%-8s", div, npc, str);
